730b97670ce2f1486e25473782f2c58c681b3f87
Jackson Bailey <jacksonbailey2122@gmail.com>
2022-09-15 10:47:03 -0500

    Convert part a inputs to bool

--- boolean_expressions.py
+++ boolean_expressions.py
@@ -13,3 +13,42 @@
 a = input('Enter True or False for a: ')
 b = input('Enter True or False for b: ')
 c = input('Enter True or False for c: ')
+# Convert a var from str to bool
+if a == 'True':
+    a = True
+elif a == 'T':
+    a = True
+elif a == 't':
+    a = True
+elif a == 'False':
+    a = False
+elif a == 'F':
+    a = False
+elif a == 'f':
+    a = False
+# Convert b var from str to bool
+if b == 'True':
+    b = True
+elif b == 'T':
+    b = True
+elif b == 't':
+    b = True
+elif b == 'False':
+    b = False
+elif b == 'F':
+    b = False
+elif b == 'f':
+    b = False
+# Convert c var from str to bool
+if c == 'True':
+    c = True
+elif c == 'T':
+    c = True
+elif c == 't':
+    c = True
+elif c == 'False':
+    c = False
+elif c == 'F':
+    c = False
+elif c == 'f':
+    c = False
