a48dee380f406a31f043f6e30f75f634ba2a09fb
Jackson Bailey <jacksonbailey2122@gmail.com>
2022-09-15 10:06:43 -0500

    Initial commit

---
+++ boolean_expressions.py
@@ -0,0 +1,11 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Names:        Jackson Bailey
+#               Colby Clayton
+#               Chris Debetta
+#               Charlie Robertson
+# Section:      562
+# Assignment:   Lab 4.15
+# Date:         23 Sep 2022
